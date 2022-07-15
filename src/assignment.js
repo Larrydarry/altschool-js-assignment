42d0199188349e52d7537763f5fc8a496f824f66
Atala Omolara <109248882+Larrydarry@users.noreply.github.com>
2022-07-15 16:10:49 +0000

    Altschool java script assignment

--- src/assignment.js
+++ src/assignment.js
@@ -12,11 +12,15 @@ const assignment = {};
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
- */
+ **/
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let numSum = arrayOfNumbers.reduce((curr, prev) => curr + prev);
+
+      return numSum;
+  
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+  assignment.sumOfNumbers = sumOfNumbers;
+
 
 /**
  * Challenge - 2
@@ -26,14 +30,16 @@ function sumOfNumbers(arrayOfNumbers) {
  * 
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
- */
+ **/
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.countEvenNumbers = countEvenNumbers;
+   let evenNumbers = arrayOfNumbers.filter(element => element % 2 == 0);
 
-/**
- * Challenge - 3
+   return evenNumbers.length;
+}
+assignment.countEvenNumbers = countEvenNumbers;
+
+
+/* * Challenge - 3
  * 
  * Given an array of numbers representing temperatures in Celsius,
  * convert each number to Fahrenheit and return a new array containing the converted
@@ -47,9 +53,10 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+    let result = arrayOfNumbers.map(e => Math.trunc(e * 1.8 + 32))
+     return result;
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+ assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
